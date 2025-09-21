0fdf2cf2d007885f4e6ccd299780c5e674a2d187
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-09-21 23:13:24 +0200

    deleted:    ai-stack/ai-services/DATA/Open-LLM-VTuber 	deleted:    ai-stack/ai-services/DATA/SwarmUI 	deleted:    ai-stack/ai-services/DATA/stable-diffusion-webui-docker 	deleted:    ai-stack/ai-stack/DATA/stable-diffusion-webui-docker 	modified:   install-stack.sh 	modified:   scripts/clone_repos.sh

--- scripts/clone_repos.sh
+++ scripts/clone_repos.sh
@@ -5,32 +5,39 @@ export LETTA_SANDBOX_MOUNT_PATH="${WD}/letta"
 export UV_LINK_MODE=copy
 
 echo "Working directory is set to ${WD}"
-# cd "${WD}" || exit
+cd "${WD}" || exit
+mkdir -p ../ai-stack/DATA
 
-mkdir -p "../ai-stack/DATA" 
 function CLONE_OLLMVT(){
-    git clone --recursive https://github.com/Open-LLM-VTuber/Open-LLM-VTuber.git ../ai-stack/DATA/Open-LLM-VTuber  &>/dev/null
-    cd ../ai-stack/DATA/Open-LLM-VTuber || exit
+
+    cd ../ai-stack/DATA || exit 1
+
+    git clone --recursive https://github.com/Open-LLM-VTuber/Open-LLM-VTuber.git Open-LLM-VTuber  &>/dev/null
+    cd Open-LLM-VTuber || exit
     uv sync
 }
 
 function CLONE_SWARMUI(){
-    git clone --recursive https://github.com/mcmonkeyprojects/SwarmUI.git ../ai-stack/DATA/SwarmUI  &>/dev/null
-    # cd  ../../././ || exit 1
-    # echo $PWD
-    cp -f CustomDockerfile-swarmui "../ai-stack/DATA/SwarmUI/launchtools/CustomDockerfile.docker"
-    cp -f custom-launch-docker.sh "../ai-stack/DATA/SwarmUI/launchtools/custom-launch-docker.sh"
+
+    cd ../ai-stack/DATA || exit 1
+
+    git clone --recursive https://github.com/mcmonkeyprojects/SwarmUI.git SwarmUI  &>/dev/null
+    cp -f CustomDockerfile-swarmui SwarmUI/launchtools/CustomDockerfile.docker
+    cp -f custom-launch-docker.sh SwarmUI/launchtools/custom-launch-docker.sh
 
     docker stop swarmui
     docker rm swarmui
 
-    ../../../ai-stack/DATA/SwarmUI/launchtools/custom-launch-docker.sh fixch
-    ../../../ai-stack/DATA/SwarmUI/launchtools/custom-launch-docker.sh
+    ./SwarmUI/launchtools/custom-launch-docker.sh fixch
+    ./SwarmUI/launchtools/custom-launch-docker.sh
 }
 
 function CLONE_STABLE-DIFFUSION-WEBUI-DOCKER(){
-    git clone --recursive https://github.com/AbdBarho/stable-diffusion-webui-docker.git ../ai-stack/DATA/stable-diffusion-webui-docker  &>/dev/null
-    mkdir -p ../ai-stack/DATA/stable-diffusion-webui-docker/data/models/CLIPEncoder
+
+    cd ../ai-stack/DATA || exit 1
+
+    git clone --recursive https://github.com/AbdBarho/stable-diffusion-webui-docker.git stable-diffusion-webui-docker  &>/dev/null
+    mkdir -p stable-diffusion-webui-docker/data/models/CLIPEncoder
 }
 CLONE_OLLMVT
 CLONE_SWARMUI
