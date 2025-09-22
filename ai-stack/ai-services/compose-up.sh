2ab3ec18132e2df7c5fb76794c353a080b0ca4e4
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-09-22 10:33:09 +0200

    modified:   ai-stack/ai-services/compose-up.sh 	modified:   management-stack/management-services/compose-up.sh 	modified:   media-stack/media-services/compose-up.sh

--- ai-stack/ai-services/compose-up.sh
+++ ai-stack/ai-services/compose-up.sh
@@ -38,5 +38,5 @@ done
 echo "Running: docker compose $ARGS up -d"
 
 docker login
-docker compose $ARGS up -d
-# docker compose $ARGS up -d --build --force-recreate --remove-orphans
+# docker compose $ARGS up -d
+docker compose $ARGS up -d --build --force-recreate --remove-orphans
