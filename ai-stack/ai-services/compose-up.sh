c5a7aaf0541cc21a84ca8244e678d0acaa1162f2
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-09-23 08:18:30 +0200

    modified:   ai-stack/ai-services/.env.librechat-mongodb 	modified:   ai-stack/ai-services/base.docker-compose.yaml 	modified:   ai-stack/ai-services/compose-up.sh 	modified:   ai-stack/ai-services/librechat.docker-compose.yaml 	modified:   management-stack/management-services/compose-up.sh 	renamed:    ai-stack/ai-services/stacks.code-workspace -> stacks.code-workspace

--- ai-stack/ai-services/compose-up.sh
+++ ai-stack/ai-services/compose-up.sh
@@ -16,6 +16,10 @@ COMPOSE_FILES=(
   letta-mcp.docker-compose.yaml
   letta-server.docker-compose.yaml
   librechat.docker-compose.yaml
+  librechat-meilisearch.docker-compose.yaml 
+  librechat-mongodb.docker-compose.yaml 
+  librechat-rag_api.docker-compose.yaml 
+  librechat-vectordb.docker-compose.yaml
   libretranslate.docker-compose.yaml
   mongo.docker-compose.yaml
   ollama.docker-compose.yaml
