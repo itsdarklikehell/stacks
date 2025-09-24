5f65d9512982feb914100b6c7eddfb76b7d18e1b
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-09-24 09:32:58 +0200

    modified:   ai-stack/ai-services/.env.basic-memory 	modified:   ai-stack/ai-services/.env.chroma 	modified:   ai-stack/ai-services/basic-memory.docker-compose.yaml 	modified:   ai-stack/ai-services/chroma.docker-compose.yaml 	modified:   ai-stack/ai-services/clickhouse.docker-compose.yaml 	modified:   management-stack/management-services/.env.ubuntu-desktop-lxde-vnc 	new file:   media-stack/media-services/.env.wolf 	modified:   media-stack/media-services/compose-up.sh 	new file:   media-stack/media-services/wolf.docker-compose.yaml 	modified:   scripts/clone_repos.sh

--- scripts/clone_repos.sh
+++ scripts/clone_repos.sh
@@ -35,8 +35,8 @@ function CLONE_SWARMUI(){
     cp -f "${WD}/CustomDockerfile-swarmui" launchtools/CustomDockerfile.docker
     cp -f "${WD}/custom-launch-docker.sh" launchtools/custom-launch-docker.sh
 
-    # ./launchtools/custom-launch-docker.sh fixch
-    # ./launchtools/custom-launch-docker.sh
+    ./launchtools/custom-launch-docker.sh fixch
+    ./launchtools/custom-launch-docker.sh
 }
 function CLONE_STABLE-DIFFUSION-WEBUI-DOCKER(){
     cd "${WD}" || exit
