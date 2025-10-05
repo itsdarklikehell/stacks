47c34fbb0e1ddda642f65ebf54518d675ce0a6bd
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-10-05 12:11:45 +0200

    modified:   scripts/CustomDockerfile-jaison-core 	modified:   scripts/create_networks.sh

--- scripts/create_networks.sh
+++ scripts/create_networks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export DEVICE="enp2s0"
 # Create Docker networks for the AI stack
 function CREATE_NETWORKS(){
     # docker network rm ai-stack management-stack kuma_network iot_macvlan
@@ -10,8 +11,8 @@ function CREATE_NETWORKS(){
 
     docker network create -d iot_macvlan \
     --subnet=192.168.1.0/24 \
-    --gateway=192.168.1.1 \
-    -o parent=enp2s0 \
+    --gateway="$(ip route show 0.0.0.0/0 dev "${DEVICE}" | cut -d\  -f3)" \
+    -o parent="${DEVICE}" \
     iot_macvlan
 }
 
