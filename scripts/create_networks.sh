11b1595a4e53e8f9200294b9ac985ba167c648e4
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-10-07 02:25:41 +0200

    modified:   scripts/create_networks.sh

--- scripts/create_networks.sh
+++ scripts/create_networks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-export DEVICE="enp2s0"
+
+export DEVICE=$(ip -oneline -4 addr show scope global | tr -s ' ' | tr '/' ' ' | cut -f 2,4 -d ' ' | head -n 1 | awk '{ print $1 }')
+export IPGATEWAY=$(ip -oneline -4 addr show scope global | tr -s ' ' | tr '/' ' ' | cut -f 2,4 -d ' ' | head -n 1 | awk '{ print $2 }' | cut -f 1-3 -d '.')
+
 # Create Docker networks for the AI stack
 function CREATE_NETWORKS(){
     # docker network rm ai-stack management-stack kuma_network macvlan
@@ -12,7 +15,7 @@ function CREATE_NETWORKS(){
     docker network create openllm-vtuber-services
 
     docker network create -d macvlan \
-    --subnet=192.168.1.0/24 \
+    --subnet="${IPGATEWAY}.0/24" \
     --gateway="$(ip route show 0.0.0.0/0 dev "${DEVICE}" | cut -d\  -f3)" \
     -o parent="${DEVICE}" \
     macvlan
