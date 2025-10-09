1db2fb3e8b69363823026b2cfb6402bcfb9da93d
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-10-09 09:23:37 +0200

    modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -92,17 +92,17 @@ echo ""
 INSTALL_AI_STACK
 echo ""
 
-echo ""
-INSTALL_ARR_STACK
-echo ""
+# echo ""
+# INSTALL_ARR_STACK
+# echo ""
 
-echo ""
-INSTALL_MANAGEMENT_STACK
-echo ""
+# echo ""
+# INSTALL_MANAGEMENT_STACK
+# echo ""
 
-echo ""
-INSTALL_MEDIA_STACK
-echo ""
+# echo ""
+# INSTALL_MEDIA_STACK
+# echo ""
 
 echo ""
 INSTALL_AIRI_STACK
