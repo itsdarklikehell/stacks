aee2b3c281cce3556b71b84720e5e120e573dcb6
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-10-14 11:34:00 +0200

    modified:   scripts/install_toolhive.sh

--- scripts/install_toolhive.sh
+++ scripts/install_toolhive.sh
@@ -6,8 +6,8 @@ function INSTALL_toolhive(){
     then
         echo "toolhive is already installed"
     else
-        curl -s https://api.github.com/repos/stacklok/toolhive-studio/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-        sudo dpkg -i *.deb
+        curl -s https://api.github.com/repos/stacklok/toolhive-studio/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -qi - || true
+        sudo dpkg -i ./*.deb
         rm *.deb
         echo "toolhive installation completed."
     fi
