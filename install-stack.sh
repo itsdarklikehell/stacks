4280caa6784097be70d397ffc399dc1be1417bc9
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-10-24 18:57:56 +0200

    modified:   ai-stack/ai-services/letta-server/.env 	modified:   ai-stack/ai-services/letta-server/.env.example 	modified:   ai-stack/ai-services/letta-server/docker-compose.yaml 	modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -236,8 +236,8 @@ echo ""
 # sudo chown -R "${USER}":"${USER}" "${WD}"
 echo "Installation complete.."
 
-# if [[ -f "${HOME}/bin/start_ai.sh" ]]; then
-# 	# gnome-terminal -- "${HOME}/bin/start_ai.sh"
-# 	xdg-open "http://localhost:8080"  # open-webui
-# 	xdg-open "http://localhost:12393" # openllm-vtuber
-# fi
+if [[ -f "${HOME}/bin/start_ai.sh" ]]; then
+	# gnome-terminal -- "${HOME}/bin/start_ai.sh"
+	xdg-open "http://localhost:8080"  # open-webui
+	xdg-open "http://localhost:12393" # openllm-vtuber
+fi
