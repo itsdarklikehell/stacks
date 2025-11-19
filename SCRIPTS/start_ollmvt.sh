7e5652379716172b94615ea3c5c824847b3d3e6c
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-11-19 07:02:26 +0100

    modified:   SCRIPTS/start_ollmvt.sh

--- SCRIPTS/start_ollmvt.sh
+++ SCRIPTS/start_ollmvt.sh
@@ -7,7 +7,7 @@ RUN_OLLMVTUBER() {
 	sudo chown -R rizzo:rizzo /media/rizzo/RAIDSTATION/stacks/DATA/openllm-vtuber-stack/openllm-vtuber
 
 	cd /media/rizzo/RAIDSTATION/stacks/DATA/openllm-vtuber-stack/openllm-vtuber || exit 1
-
+	source .venv/bin/activate
 	uv run run_server.py
 }
 
