dddebbabb6b009ead1f5397d5bd252ffd37bdc37
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-12-06 06:17:16 +0100

    modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -9,7 +9,7 @@ export SECRETS_DIR="${WD}/SECRETS"               # folder that store secrets
 export PERM_DATA="${WD}/DATA"                    # folders that store stack data
 export CONFIGS_DIR="${WD}/STACKS"                # folders that store stack configs
 export CLEANUP="false"                           # false, true
-export PRUNE="false"                             # false, true/normal, all
+export PRUNE="true"                              # false, true/normal, all
 export BUILDING="true"                           # false, true, force_rebuild
 export PULL_MODELS="true"                        # false, true
 export START_OLLMVT="true"                       # false, true
@@ -151,11 +151,11 @@ INSTALL_DOCKER
 CLEANUP_DATA
 PRUNING
 
-echo ""
-echo "Cloning repos"
-echo ""
-CLONE_REPOS # >/dev/null 2>&1
-echo ""
+# echo ""
+# echo "Cloning repos"
+# echo ""
+# CLONE_REPOS # >/dev/null 2>&1
+# echo ""
 
 ## STACKS:
 CREATE_NETWORKS
