d57d522055b72a3001c1e9269a84414438c7729b
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-12-09 11:37:22 +0100

    modified:   SCRIPTS/pull_models.sh 	modified:   SCRIPTS/start_comfyui.sh

--- SCRIPTS/start_comfyui.sh
+++ SCRIPTS/start_comfyui.sh
@@ -5,9 +5,39 @@ export UV_LINK_MODE=copy
 export BACKGROUND=false
 export COMFYUI_PORT=8188
 
+export REPAIR=true
+
 export COMFY_PATH="/media/rizzo/RAIDSTATION/stacks/DATA/ai-stack/comfyui"
 
 export COMFYUI_MODEL_PATH="${COMFY_PATH}/models"
+
+ln -sf "${COMFYUI_MODEL_PATH}" "${COMFY_PATH}/custom_nodes/models"
+
+function REPAIR_COMFYUI() {
+	if [[ ${REPAIR} == "true" ]]; then
+		echo "Repairing ComfyUI installation..."
+		echo ""
+
+		for dir in "${COMFY_PATH}/custom_nodes"/*; do
+			echo "Checking custom node: ${dir}"
+			if [[ -f "${dir}/install.py" ]]; then
+				echo "Reinstalling dependencies for custom node: ${dir}"
+				uv run python "${dir}/install.py" --force
+				echo ""
+			elif [[ -f "${dir}/requirements.txt" ]]; then
+				echo "Reinstalling requirements for custom node: ${dir}"
+				uv pip install -r "${dir}/requirements.txt"
+				echo ""
+			fi
+		done
+
+		# echo "Installing additional requirements..."
+		# find "${COMFY_PATH}" -type f -name "install.py" -exec uv run python "{}" \;
+		# find "${COMFY_PATH}" -type f -name "requirements.txt" -exec uv pip install -r "{}" \;
+		# echo ""
+		uv run comfy-cli update all
+	fi
+}
 
 RUN_COMFYUI() {
 
@@ -25,16 +55,17 @@ RUN_COMFYUI() {
 
 		uv pip install comfy-cli
 		uv run comfy-cli install --nvidia --restore
-		uv run comfy-cli update all
 	fi
+
+	echo ""
+	REPAIR_COMFYUI
+	echo ""
 
 	if [[ ${BACKGROUND} == "true" ]]; then
 		echo "Starting ComfyUI in background mode..."
-		uv run comfy-cli update all
 		uv run comfy-cli launch --background -- --listen "0.0.0.0" --port "${COMFYUI_PORT}"
 	else
 		echo "Starting ComfyUI in foreground mode..."
-		uv run comfy-cli update all
 		uv run comfy-cli launch --no-background -- --listen "0.0.0.0" --port "${COMFYUI_PORT}"
 	fi
 }
