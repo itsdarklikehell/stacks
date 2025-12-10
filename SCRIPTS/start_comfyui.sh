f99cb8cb4942cc4bb7d2ad9a396544983590f57f
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-12-10 01:21:05 +0100

    modified:   SCRIPTS/clone_repos.sh 	deleted:    SCRIPTS/comfyui_custom_nodes.txt 	new file:   SCRIPTS/comfyui_essential_custom_nodes.txt 	new file:   SCRIPTS/comfyui_extra_custom_nodes.txt 	modified:   SCRIPTS/start_comfyui.sh

--- SCRIPTS/start_comfyui.sh
+++ SCRIPTS/start_comfyui.sh
@@ -6,6 +6,10 @@ export BACKGROUND=false
 export COMFYUI_PORT=8188
 
 export STACK_BASEPATH="/media/rizzo/RAIDSTATION/stacks"
+
+export ESSENTIAL_CUSTOM_NODELIST="${STACK_BASEPATH}/SCRIPTS/comfyui_essential_custom_nodes.txt"
+export EXTRA_CUSTOM_NODELIST="${STACK_BASEPATH}/SCRIPTS/comfyui_extra_custom_nodes.txt"
+
 export COMFYUI_PATH="${STACK_BASEPATH}/DATA/ai-stack/comfyui"
 export COMFYUI_MODEL_PATH="${COMFYUI_PATH}/models"
 
@@ -21,58 +25,65 @@ elif [[ -z "$1" ]] || [[ "$1" == "-nr" ]] || [[ "$1" == "--no-repair" ]]; then
 	export REPAIR=false
 fi
 
+echo "Cloning comfyui"
+echo ""
+git clone --recursive https://github.com/comfyanonymous/ComfyUI.git "${COMFYUI_PATH}"
+cd "${COMFYUI_PATH}" || exit 1
+
 ln -sf "${COMFYUI_MODEL_PATH}" "${COMFYUI_PATH}/custom_nodes/"
 
 function TRY_REPAIR_COMFYUI() {
-	if [[ "${REPAIR}" == "true" ]]; then
-
-		if [[ -f "${COMFYUI_PATH}/custom_nodes.list" ]]; then
-			echo "Reinstalling custom nodes from custom_nodes.list"
-			while IFS= read -r node_name; do
-				if [[ -n "${node_name}" ]]; then
-					uv run comfy-cli node install "${node_name}" # --force
-				fi
-			done <"${STACK_BASEPATH}/SCRIPTS/comfyui_custom_nodes.txt"
-			echo ""
-		else
-			echo "No custom_nodes.list file found."
-		fi
-
-		# for dir in "${COMFYUI_PATH}"/custom_nodes/*; do
-		# 	if [[ -d "${dir}" && ! -L "${dir}" ]]; then
-
-		# 		node_name=$(basename "${dir}")
-		# 		echo ""
-		# 		echo "Reinstalling custom node: ${node_name}"
-		# 		uv run comfy-cli node install "${node_name}"
-
-		# 		if [[ -f "${dir}/install.py" ]]; then
-		# 			echo ""
-		# 			echo "Reinstalling dependencies for custom node: ${dir} using install.py"
-		# 			uv run python "${dir}/install.py"
-		# 			echo ""
-		# 		elif [[ -f "${dir}/requirements.txt" ]]; then
-		# 			echo ""
-		# 			echo "Reinstalling requirements for custom node: ${dir} using requirements.txt"
-		# 			uv pip install -r "${dir}/requirements.txt"
-		# 			echo ""
-		# 		# elif [[ -f "${dir}/pyproject.toml" ]]; then
-		# 		# 	echo ""
-		# 		# 	echo "Reinstalling package for custom node: ${dir} using pyproject.toml"
-		# 		# 	uv pip install -e "${dir}"
-		# 		# 	echo ""
-		# 		else
-		# 			echo "No install.py, requirements.txt, setup.py, or pyproject.toml found in ${dir}. Skipping."
-		# 		fi
-		# 	fi
-		# done
+	if [[ -f "${ESSENTIAL_CUSTOM_NODELIST}" ]]; then
+		echo "Reinstalling custom nodes from ${ESSENTIAL_CUSTOM_NODELIST}"
+		while IFS= read -r node_name; do
+			if [[ -n "${node_name}" ]]; then
+				uv run comfy-cli node install "${node_name}"
+			fi
+		done <"${ESSENTIAL_CUSTOM_NODELIST}"
+		echo ""
 	else
-		echo "Skipping repair of ComfyUI custom nodes."
+		echo "No ${ESSENTIAL_CUSTOM_NODELIST} file found. Skipping custom node reinstallation."
+	fi
+	if [[ -f "${EXTRA_CUSTOM_NODELIST}" ]]; then
+		echo "Reinstalling custom nodes from ${EXTRA_CUSTOM_NODELIST}"
+		while IFS= read -r node_name; do
+			if [[ -n "${node_name}" ]]; then
+				uv run comfy-cli node install "${node_name}"
+			fi
+		done <"${EXTRA_CUSTOM_NODELIST}"
+		echo ""
+	else
+		echo "No ${EXTRA_CUSTOM_NODELIST} file found. Skipping custom node reinstallation."
 	fi
 
 	uv run comfy-cli update all
 }
 
+function LOCAL_SETUP() {
+	echo "Using Local setup"
+	# ./install.sh
+
+	if [[ -f .venv/bin/activate ]]; then
+		source .venv/bin/activate
+	else
+		export UV_LINK_MODE=copy
+		uv venv --clear --seed
+		source .venv/bin/activate
+
+		uv pip install --upgrade pip
+		uv sync --all-extras
+
+		uv pip install comfy-cli
+		yes | uv run comfy-cli install --nvidia --restore || true
+	fi
+}
+function DOCKER_SETUP() {
+	echo "Using Docker setup"
+	# cp -f "${WD}/CustomDockerfile-whisperx-uv" CustomDockerfile-whisperx-uv
+	# cp -f "${WD}/CustomDockerfile-whisperx-conda" CustomDockerfile-whisperx-conda
+	# cp -f "${WD}/CustomDockerfile-whisperx-venv" CustomDockerfile-whisperx-venv
+	# docker build -t whisperx .
+}
 function RUN_COMFYUI() {
 
 	cd "${COMFYUI_PATH}" || exit 1
@@ -93,10 +104,6 @@ function RUN_COMFYUI() {
 		echo "ComfyUI virtual environment created and dependencies installed."
 	fi
 
-	echo ""
-	TRY_REPAIR_COMFYUI
-	echo ""
-
 	if [[ ${BACKGROUND} == "true" ]]; then
 		echo "Starting ComfyUI in background mode..."
 		uv run comfy-cli launch --background -- --listen "0.0.0.0" --port "${COMFYUI_PORT}"
@@ -106,4 +113,7 @@ function RUN_COMFYUI() {
 	fi
 }
 
+LOCAL_SETUP        # >/dev/null 2>&1 &
+DOCKER_SETUP       # >/dev/null 2>&1 &
+TRY_REPAIR_COMFYUI # >/dev/null 2>&1 &
 RUN_COMFYUI
