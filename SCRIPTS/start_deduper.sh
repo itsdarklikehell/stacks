2078d51cdb711be57e2589d6301939b892ae3d73
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-12-13 09:37:05 +0100

    modified:   SCRIPTS/start_deduper.sh 	new file:   SCRIPTS/tadaa.wav

--- SCRIPTS/start_deduper.sh
+++ SCRIPTS/start_deduper.sh
@@ -12,7 +12,9 @@ export AI_INPUTS_PATH="/${STACK_BASEPATH}/DATA/ai-inputs"
 export AI_OUTPUTS_PATH="/${STACK_BASEPATH}/DATA/ai-outputs"
 export AI_WORKFLOWS_PATH="/${STACK_BASEPATH}/DATA/ai-workflows"
 
-sudo apt install -y rmlint rmlint-gui
+sudo apt install -y rmlint rmlint-gui vlc >/dev/null 2>&1 &
+
+cd "${STACK_BASEPATH}/SCRIPTS" || exit 1
 
 folders=(
 	"${AI_MODELS_PATH}"
@@ -22,18 +24,16 @@ folders=(
 )
 
 for folder in "${folders[@]}"; do
-	echo "Deduping ${folder}"
-
-	rmlint -g -o sh:rmlint.sh -c sh:symlink "${folder}"
-	./rmlint.sh -dpc
-
+	echo "Deduplicating ${folder}"
+	rmlint -g -o sh:rmlint.sh -c sh:symlink "${folder}" >/dev/null 2>&1 &
+	"${WD}/rmlint.sh" -dc >/dev/null 2>&1 &
 	if [[ -z "${DO_REMOVE}" ]]; then
-		rm -f 'rmlint.sh'
-		rm -f 'rmlint.json'
+		rm -f "rmlint.sh"
+		rm -f "rmlint.json"
 	fi
+	echo "Deduplicated ${folder}"
 done
 
-sudo apt install -y vlc
 wavfile=https://www.winhistory.de/more/winstart/down/owin31.wav
 wget -c "${wavfile}" -O "tadaa.wav" >/dev/null 2>&1 &
 cvlc tadaa.wav >/dev/null 2>&1 &
