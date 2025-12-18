ab5d5d56b9004857f7c9cd1d2d7b7448a3fce944
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-12-18 18:31:14 +0100

    modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -248,40 +248,40 @@ echo ""
 SETUP_AUTOSTART
 echo ""
 
-# echo ""
-# SETUP_ESSENTIALS_STACK
-# echo ""
+echo ""
+SETUP_ESSENTIALS_STACK
+echo ""
 
 echo ""
 SETUP_AI_STACK
 echo ""
 
-# echo ""
-# PULL_MODELS >/dev/null 2>&1 &
-# echo ""
+echo ""
+PULL_MODELS >/dev/null 2>&1 &
+echo ""
 
-# echo ""
-# START_COMFYUI >/dev/null 2>&1 &
-# echo ""
+echo ""
+START_COMFYUI >/dev/null 2>&1 &
+echo ""
 
-# echo ""
-# START_CUSHYSTUDIO >/dev/null 2>&1 &
-# echo ""
+echo ""
+START_CUSHYSTUDIO >/dev/null 2>&1 &
+echo ""
 
-# echo ""
-# START_OLLMVT >/dev/null 2>&1 &
-# echo ""
+echo ""
+START_OLLMVT >/dev/null 2>&1 &
+echo ""
 
-# echo ""
-# sleep 3
-# START_DEDUPER >/dev/null 2>&1 &
-# echo ""
+echo ""
+sleep 3
+START_DEDUPER >/dev/null 2>&1 &
+echo ""
 
-# echo ""
-# START_BROWSER >/dev/null 2>&1 &
-# echo ""
+echo ""
+START_BROWSER >/dev/null 2>&1 &
+echo ""
 
-# "${STACK_BASEPATH}"/SCRIPTS/done_sound.sh
+"${STACK_BASEPATH}"/SCRIPTS/done_sound.sh
 
 echo "Installation complete.."
 
