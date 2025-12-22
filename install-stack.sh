b3dc7ca2abfb0b651b42a739b02caafa203a5830
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-12-22 23:53:32 +0100

    modified:   STACKS/essential-stack/essential-services/immich-server/.env 	modified:   STACKS/essential-stack/essential-services/immich-server/docker-compose.yaml 	modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -271,11 +271,11 @@ INSTALL_DOCKER
 CLEANUP_DATA
 PRUNING
 
-echo ""
-echo "Cloning repos"
-echo ""
-CLONE_REPOS # >/dev/null 2>&1
-echo ""
+# echo ""
+# echo "Cloning repos"
+# echo ""
+# CLONE_REPOS # >/dev/null 2>&1
+# echo ""
 
 ### STACKS:
 CREATE_SECRETS
