e56cd0936de5ecbbaca2f217a74a61cfc2fdbee4
Bauke Molenaar <bauke.molenaar@gmail.com>
2026-01-06 15:00:56 +0100

    modified:   SCRIPTS/start_browser_games.sh

--- SCRIPTS/start_browser_games.sh
+++ SCRIPTS/start_browser_games.sh
@@ -10,21 +10,21 @@ export IP_ADDRESS=0.0.0.0
 
 URLS=(
 	"https://${IP_ADDRESS}:7001" # azahar
-	"http://${IP_ADDRESS}:7002"  # citron
-	"http://${IP_ADDRESS}:7008"  # dolphin
-	"http://${IP_ADDRESS}:7010"  # dosbox
-	"http://${IP_ADDRESS}:7012"  # duckstation
-	"http://${IP_ADDRESS}:7014"  # flycast
-	"http://${IP_ADDRESS}:7020"  # luanti
-	"http://${IP_ADDRESS}:7022"  # mame
-	"http://${IP_ADDRESS}:7024"  # melonds
-	"http://${IP_ADDRESS}:7026"  # modmanager
-	"http://${IP_ADDRESS}:7028"  # modrinth
-	"http://${IP_ADDRESS}:7030"  # pcsx2
-	"http://${IP_ADDRESS}:7032"  # retroarch
-	"http://${IP_ADDRESS}:7034"  # rpcs3
-	"http://${IP_ADDRESS}:7036"  # scummvm
-	"http://${IP_ADDRESS}:7044"  # xemu
+	"https://${IP_ADDRESS}:7003"  # citron
+	"https://${IP_ADDRESS}:7009"  # dolphin
+	"https://${IP_ADDRESS}:7011"  # dosbox
+	"https://${IP_ADDRESS}:7013"  # duckstation
+	"https://${IP_ADDRESS}:7015"  # flycast
+	"http://${IP_ADDRESS}:7022"  # luanti
+	"https://${IP_ADDRESS}:7023"  # mame
+	"https://${IP_ADDRESS}:7025"  # melonds
+	"http://${IP_ADDRESS}:7028"  # modmanager
+	"https://${IP_ADDRESS}:7029"  # modrinth
+	"https://${IP_ADDRESS}:7031"  # pcsx2
+	"https://${IP_ADDRESS}:7033"  # retroarch
+	"https://${IP_ADDRESS}:7035"  # rpcs3
+	"https://${IP_ADDRESS}:7037"  # scummvm
+	"https://${IP_ADDRESS}:7045"  # xemu
 )
 
 function RUN_BROWSER() {
