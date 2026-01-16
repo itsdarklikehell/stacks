cb986d1e1f98193aa50f74d3fc0724f80a5b2024
Bauke Molenaar <bauke.molenaar@gmail.com>
2026-01-16 03:23:56 +0100

    modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -365,37 +365,37 @@ if [[ ${USER} == "hans" ]]; then
 	SETUP_DOWNLOADER_STACK
 	echo ""
 
-	echo ""
-	SETUP_AI_STACK
-	echo ""
+	# echo ""
+	# SETUP_AI_STACK
+	# echo ""
 
-	echo ""
-	SETUP_ESSENTIALS_STACK
-	echo ""
+	# echo ""
+	# SETUP_ESSENTIALS_STACK
+	# echo ""
 
-	echo ""
-	SETUP_BACKUPS_STACK
-	echo ""
+	# echo ""
+	# SETUP_BACKUPS_STACK
+	# echo ""
 
-	echo ""
-	SETUP_CHAT_STACK
-	echo ""
+	# echo ""
+	# SETUP_CHAT_STACK
+	# echo ""
 
-	echo ""
-	SETUP_BOOKS_STACK
-	echo ""
+	# echo ""
+	# SETUP_BOOKS_STACK
+	# echo ""
 
-	echo ""
-	SETUP_SDR_STACK
-	echo ""
+	# echo ""
+	# SETUP_SDR_STACK
+	# echo ""
 
 	echo ""
 	SETUP_GAMESERVER_STACK
 	echo ""
 
-	echo ""
-	SETUP_MEDIA_STACK
-	echo ""
+	# echo ""
+	# SETUP_MEDIA_STACK
+	# echo ""
 
 	# echo ""
 	# SETUP_DESKTOP_STACK
