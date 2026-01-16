e06758f7581bb2434873294e761d977a5023c5c9
Bauke Molenaar <bauke.molenaar@gmail.com>
2026-01-17 00:35:57 +0100

    modified:   STACKS/ai-stack/ai-services/InvokeAI/docker-compose.yaml 	modified:   STACKS/essential-stack/essential-services/cadvisor/docker-compose.yaml 	modified:   STACKS/essential-stack/essential-services/immich-server/docker-compose.yaml 	modified:   STACKS/essential-stack/essential-services/motioneye/docker-compose.yaml 	modified:   STACKS/essential-stack/essential-services/sablier/docker-compose.yaml 	modified:   STACKS/sdr-stack/sdr-services/openwebrxplus/docker-compose.yaml 	modified:   STACKS/sdr-stack/sdr-services/shipfeeder/docker-compose.yaml 	modified:   STACKS/sdr-stack/sdr-services/vesselalert/docker-compose.yaml 	modified:   STACKS/testing-stack/testing-services/mpd/docker-compose.yaml 	modified:   STACKS/testing-stack/testing-services/obs-studio/docker-compose.yaml 	modified:   STACKS/testing-stack/testing-services/owncast/docker-compose.yaml 	modified:   STACKS/testing-stack/testing-services/synctube/docker-compose.yaml 	modified:   STACKS/testing-stack/testing-services/wolf/docker-compose.yaml 	modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -13,7 +13,7 @@ export PERM_DATA="${STACK_BASEPATH}/DATA"                                       
 export CONFIGS_DIR="${STACK_BASEPATH}/STACKS"                                   # folders that store stack configs
 export CLEANUP="false"                                                          # false, true
 export PRUNE="true"                                                             # false, true/normal, all
-export BUILDING="force_rebuild"															# false, true, force_rebuild
+export BUILDING="true"															# false, true, force_rebuild
 export PULL_MODELS="true"                                                       # false, true
 export START_OLLMVT="true"                                                      # false, true
 export START_COMFYUI="true"                                                     # false, true
@@ -369,9 +369,9 @@ if [[ ${USER} == "hans" ]]; then
 	# SETUP_AI_STACK
 	# echo ""
 
-	# echo ""
-	# SETUP_ESSENTIALS_STACK
-	# echo ""
+	echo ""
+	SETUP_ESSENTIALS_STACK
+	echo ""
 
 	# echo ""
 	# SETUP_BACKUPS_STACK
@@ -394,9 +394,9 @@ if [[ ${USER} == "hans" ]]; then
 	# SETUP_MEDIA_STACK
 	# echo ""
 
-	echo ""
-	SETUP_SDR_STACK
-	echo ""
+	# echo ""
+	# SETUP_SDR_STACK
+	# echo ""
 	
 	# echo ""
 	# SETUP_DESKTOP_STACK
