996730175e0655cdbcdb3702fb5990222dc451db
Bauke Molenaar <bauke.molenaar@gmail.com>
2026-01-20 21:28:21 +0100

    new file:   SCRIPTS/Dockerfile-rtl-tcp 	new file:   SCRIPTS/autostart-rtl-tcp.sh 	renamed:    SCRIPTS/sdrangel-autostart.sh -> SCRIPTS/autostart-sdrangel-webtop.sh 	modified:   STACKS/sdr-stack/sdr-services/compose-up.sh

---
+++ SCRIPTS/autostart-rtl-tcp.sh
@@ -0,0 +1,8 @@
+#!/with-contenv bash
+
+# Wacht tot de grafische interface (X-server) up is
+sleep 5
+
+# Start rtl_tcp als de gebruiker 'abc'
+# We sturen de output naar /dev/null om logs schoon te houden
+sudo -u abc DISPLAY=:1 rtl_tcp >/dev/null 2>&1 &
