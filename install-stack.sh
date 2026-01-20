c55977056c9e6b76400259b704ff6f4f7a2a0247
hmol33 <hmol33@gmail.com>
2026-01-20 09:19:49 +0100

    modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -375,9 +375,9 @@ if [[ ${USER} == "hans" ]]; then
 	# SETUP_AUTOSTART
 	# echo ""
 
-	echo ""
-	SETUP_DOWNLOADER_STACK
-	echo ""
+	# echo ""
+	# SETUP_DOWNLOADER_STACK
+	# echo ""
 
 	echo ""
 	SETUP_AI_STACK
@@ -387,30 +387,30 @@ if [[ ${USER} == "hans" ]]; then
 	SETUP_OPENLLM_VTUBER_STACK
 	echo ""
 
-	echo ""
-	SETUP_ESSENTIALS_STACK
-	echo ""
+	# echo ""
+	# SETUP_ESSENTIALS_STACK
+	# echo ""
 
-	echo ""
-	SETUP_BACKUPS_STACK
-	echo ""
+	# echo ""
+	# SETUP_BACKUPS_STACK
+	# echo ""
 
-	echo ""
-	SETUP_CHAT_STACK
-	echo ""
+	# echo ""
+	# SETUP_CHAT_STACK
+	# echo ""
 
-	echo ""
-	SETUP_BOOKS_STACK
-	echo ""
+	# echo ""
+	# SETUP_BOOKS_STACK
+	# echo ""
 
-	echo ""
-	SETUP_GAMESERVER_STACK
-	# # docker compose exec pterodactyl-panel php artisan p:user:make
-	echo ""
+	# echo ""
+	# SETUP_GAMESERVER_STACK
+	# # # docker compose exec pterodactyl-panel php artisan p:user:make
+	# echo ""
 
-	echo ""
-	SETUP_MEDIA_STACK
-	echo ""
+	# echo ""
+	# SETUP_MEDIA_STACK
+	# echo ""
 
 	# echo ""
 	# SETUP_SDR_STACK
