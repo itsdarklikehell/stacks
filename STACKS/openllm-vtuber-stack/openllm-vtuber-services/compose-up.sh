6cdb2bf2d80cc66dcb177feaf3e5e43516cc96cd
Bauke Molenaar <bauke.molenaar@gmail.com>
2026-01-20 03:32:26 +0100

    renamed:    SCRIPTS/characters-openllm/README.md -> SCRIPTS/characters-openllm-vtuber/README.md 	renamed:    SCRIPTS/characters-openllm/archivist_ink_008.yaml -> SCRIPTS/characters-openllm-vtuber/archivist_ink_008.yaml 	renamed:    SCRIPTS/characters-openllm/en_echo_7.yaml -> SCRIPTS/characters-openllm-vtuber/en_echo_7.yaml 	renamed:    SCRIPTS/characters-openllm/en_mili.yaml -> SCRIPTS/characters-openllm-vtuber/en_mili.yaml 	renamed:    SCRIPTS/characters-openllm/en_nuke_debate.yaml -> SCRIPTS/characters-openllm-vtuber/en_nuke_debate.yaml 	renamed:    SCRIPTS/characters-openllm/en_professor_paws.yaml -> SCRIPTS/characters-openllm-vtuber/en_professor_paws.yaml 	renamed:    SCRIPTS/characters-openllm/en_translate_tone.yaml -> SCRIPTS/characters-openllm-vtuber/en_translate_tone.yaml 	renamed:    SCRIPTS/characters-openllm/en_unhelpful_ai.yaml -> SCRIPTS/characters-openllm-vtuber/en_unhelpful_ai.yaml 	renamed:    SCRIPTS/characters-openllm/lumi_volt_009_electro.yaml -> SCRIPTS/characters-openllm-vtuber/lumi_volt_009_electro.yaml 	renamed:    SCRIPTS/characters-openllm/noir_monochrome_40.yaml -> SCRIPTS/characters-openllm-vtuber/noir_monochrome_40.yaml 	renamed:    SCRIPTS/characters-openllm/rose_042_scavenger.yaml -> SCRIPTS/characters-openllm-vtuber/rose_042_scavenger.yaml 	renamed:    SCRIPTS/characters-openllm/suds_99_alchemist.yaml -> SCRIPTS/characters-openllm-vtuber/suds_99_alchemist.yaml 	renamed:    "SCRIPTS/characters-openllm/zh_\347\261\263\347\262\222.yaml" -> "SCRIPTS/characters-openllm-vtuber/zh_\347\261\263\347\262\222.yaml" 	renamed:    "SCRIPTS/characters-openllm/zh_\347\277\273\350\257\221\350\205\224.yaml" -> "SCRIPTS/characters-openllm-vtuber/zh_\347\277\273\350\257\221\350\205\224.yaml" 	new file:   SCRIPTS/prompts-openllm-vtuber/README.md 	new file:   SCRIPTS/prompts-openllm-vtuber/__init__.py 	new file:   SCRIPTS/prompts-openllm-vtuber/prompt_loader.py 	new file:   SCRIPTS/prompts-openllm-vtuber/utils/concise_style_prompt.txt 	new file:   SCRIPTS/prompts-openllm-vtuber/utils/group_conversation_prompt.txt 	new file:   SCRIPTS/prompts-openllm-vtuber/utils/live2d_expression_prompt.txt 	new file:   SCRIPTS/prompts-openllm-vtuber/utils/live_prompt.txt 	new file:   SCRIPTS/prompts-openllm-vtuber/utils/mcp_prompt.txt 	new file:   SCRIPTS/prompts-openllm-vtuber/utils/proactive_speak_prompt.txt 	new file:   SCRIPTS/prompts-openllm-vtuber/utils/speakable_prompt.txt 	new file:   SCRIPTS/prompts-openllm-vtuber/utils/think_tag_prompt.txt 	new file:   SCRIPTS/prompts-openllm-vtuber/utils/tool_guidance_prompt.txt 	modified:   STACKS/openllm-vtuber-stack/openllm-vtuber-services/compose-up.sh

--- STACKS/openllm-vtuber-stack/openllm-vtuber-services/compose-up.sh
+++ STACKS/openllm-vtuber-stack/openllm-vtuber-services/compose-up.sh
@@ -36,6 +36,7 @@ function SETUP_FOLDERS() {
 		SOURCE_FOLDER="${STACK_BASEPATH}/DATA/${STACK_NAME}-stack/${SERVICE_NAME}" || exit 1
 
 		cp -rf "${STACK_BASEPATH}/SCRIPTS/models-${SERVICE_NAME}" "${SOURCE_FOLDER}/models"
+		cp -rf "${STACK_BASEPATH}/SCRIPTS/prompts-${SERVICE_NAME}" "${SOURCE_FOLDER}/prompts"
 		cp -rf "${STACK_BASEPATH}/SCRIPTS/characters-${SERVICE_NAME}" "${SOURCE_FOLDER}/characters"
 		cp -rf "${STACK_BASEPATH}/SCRIPTS/Dockerfile-${SERVICE_NAME}" "${SOURCE_FOLDER}/Dockerfile"
 		cp -rf "${STACK_BASEPATH}/SCRIPTS/autostart-${SERVICE_NAME}.sh" "${SOURCE_FOLDER}/autostart.sh"
@@ -44,6 +45,10 @@ function SETUP_FOLDERS() {
 
 		if [[ ! -d "${FOLDER}/models" ]]; then
 			cp -rf "${STACK_BASEPATH}/SCRIPTS/models-${SERVICE_NAME}" "${SOURCE_FOLDER}/models"
+		fi
+
+		if [[ ! -d "${FOLDER}/prompts" ]]; then
+			cp -rf "${STACK_BASEPATH}/SCRIPTS/prompts-${SERVICE_NAME}" "${SOURCE_FOLDER}/prompts"
 		fi
 
 		if [[ ! -d "${FOLDER}/characters" ]]; then
