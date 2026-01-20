e60c80c78a1e6797665ab17ac722aedfc0c078db
hmol33 <hmol33@gmail.com>
2026-01-20 09:54:15 +0100

    modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -387,34 +387,34 @@ if [[ ${USER} == "hans" ]]; then
 	# SETUP_OPENLLM_VTUBER_STACK
 	# echo ""
 
-	echo ""
-	SETUP_ESSENTIALS_STACK
-	echo ""
+	# echo ""
+	# SETUP_ESSENTIALS_STACK
+	# echo ""
 
-	echo ""
-	SETUP_BACKUPS_STACK
-	echo ""
+	# echo ""
+	# SETUP_BACKUPS_STACK
+	# echo ""
 
-	echo ""
-	SETUP_CHAT_STACK
-	echo ""
+	# echo ""
+	# SETUP_CHAT_STACK
+	# echo ""
 
-	echo ""
-	SETUP_BOOKS_STACK
-	echo ""
+	# echo ""
+	# SETUP_BOOKS_STACK
+	# echo ""
 
-	echo ""
-	SETUP_GAMESERVER_STACK
-	# # docker compose exec pterodactyl-panel php artisan p:user:make
-	echo ""
+	# echo ""
+	# SETUP_GAMESERVER_STACK
+	# # # docker compose exec pterodactyl-panel php artisan p:user:make
+	# echo ""
 
-	echo ""
-	SETUP_MEDIA_STACK
-	echo ""
+	# echo ""
+	# SETUP_MEDIA_STACK
+	# echo ""
 
-	echo ""
-	SETUP_SDR_STACK
-	echo ""
+	# echo ""
+	# SETUP_SDR_STACK
+	# echo ""
 
 	########################
 
