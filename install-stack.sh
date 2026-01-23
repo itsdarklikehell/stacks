e3273c4c64e4d0c7e51d824c9be99e70eaa425a8
Bauke Molenaar <bauke.molenaar@gmail.com>
2026-01-23 09:45:48 +0100

    modified:   install-stack.sh

--- install-stack.sh
+++ install-stack.sh
@@ -380,9 +380,9 @@ if [[ ${USER} == "hans" ]]; then
 	# SETUP_AUTOSTART
 	# echo ""
 
-	# echo ""
-	# SETUP_DOWNLOADER_STACK
-	# echo ""
+	echo ""
+	SETUP_DOWNLOADER_STACK
+	echo ""
 
 	echo ""
 	SETUP_AI_STACK
@@ -392,34 +392,34 @@ if [[ ${USER} == "hans" ]]; then
 	SETUP_OPENLLM_VTUBER_STACK
 	echo ""
 
-	# echo ""
-	# SETUP_ESSENTIALS_STACK
-	# echo ""
+	echo ""
+	SETUP_ESSENTIALS_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_BACKUPS_STACK
-	# echo ""
+	echo ""
+	SETUP_BACKUPS_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_CHAT_STACK
-	# echo ""
+	echo ""
+	SETUP_CHAT_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_BOOKS_STACK
-	# echo ""
+	echo ""
+	SETUP_BOOKS_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_MEDIA_STACK
-	# echo ""
+	echo ""
+	SETUP_MEDIA_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_SDR_STACK
-	# echo ""
+	echo ""
+	SETUP_SDR_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_GAMESERVER_STACK
-	# # # docker compose exec pterodactyl-panel php artisan p:user:make
-	# echo ""
+	echo ""
+	SETUP_GAMESERVER_STACK
+	# # docker compose exec pterodactyl-panel php artisan p:user:make
+	echo ""
 
 	########################
 
@@ -436,9 +436,9 @@ elif [[ ${USER} == "rizzo" ]]; then
 	# SETUP_AUTOSTART
 	# echo ""
 
-	# echo ""
-	# SETUP_DOWNLOADER_STACK
-	# echo ""
+	echo ""
+	SETUP_DOWNLOADER_STACK
+	echo ""
 
 	echo ""
 	SETUP_AI_STACK
@@ -448,34 +448,34 @@ elif [[ ${USER} == "rizzo" ]]; then
 	SETUP_OPENLLM_VTUBER_STACK
 	echo ""
 
-	# echo ""
-	# SETUP_ESSENTIALS_STACK
-	# echo ""
+	echo ""
+	SETUP_ESSENTIALS_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_BACKUPS_STACK
-	# echo ""
+	echo ""
+	SETUP_BACKUPS_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_CHAT_STACK
-	# echo ""
+	echo ""
+	SETUP_CHAT_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_BOOKS_STACK
-	# echo ""
+	echo ""
+	SETUP_BOOKS_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_MEDIA_STACK
-	# echo ""
+	echo ""
+	SETUP_MEDIA_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_SDR_STACK
-	# echo ""
+	echo ""
+	SETUP_SDR_STACK
+	echo ""
 
-	# echo ""
-	# SETUP_GAMESERVER_STACK
-	# # # docker compose exec pterodactyl-panel php artisan p:user:make
-	# echo ""
+	echo ""
+	SETUP_GAMESERVER_STACK
+	# # docker compose exec pterodactyl-panel php artisan p:user:make
+	echo ""
 
 	########################
 
