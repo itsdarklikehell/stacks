d15df3f1fd1fa14164c854cffcbd2b38c41936e9
Bauke Molenaar <bauke.molenaar@gmail.com>
2026-03-04 02:20:21 +0100

    modified:   SCRIPTS/characters-openllm-vtuber/en_professor_paws.yaml 	modified:   SCRIPTS/install_openclaw.sh

--- SCRIPTS/install_openclaw.sh
+++ SCRIPTS/install_openclaw.sh
@@ -3,24 +3,69 @@
 
 echo "Install openclaw script started."
 
+function UNINSTALL_OPENCLAW() {
+	systemctl --user stop openclaw-gateway.service
+	systemctl --user disable openclaw-gateway.service
+	rm -f ~/.config/systemd/user/openclaw-gateway.service
+	systemctl --user daemon-reload
+
+	if command -v openclaw &>/dev/null; then
+		openclaw gateway stop
+		openclaw uninstall --all --yes --non-interactive
+	fi
+
+	npx -y openclaw uninstall --all --yes --non-interactive
+	npm rm -g openclaw
+	pnpm remove -g openclaw
+	bun remove -g openclaw
+
+	sudo rm -rf "${OPENCLAW_STATE_DIR:-$HOME/.openclaw}"
+	sudo rm -rf ~/.openclaw/workspace
+	sudo rm -rf ~/.openclaw-*
+
+}
+
 function INSTALL_OPENCLAW() {
 	curl -o- https://deb.nodesource.com/setup_25.x | bash
-	sudo apt install nodejs
+	sudo apt install -y nodejs
 
 	# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 	source ~/.bashrc
 	# nvm install node
 	source ~/.bashrc
+
+	if [[ ! -d ~/openclaw ]]; then
+		git clone --recursive https://github.com/openclaw/openclaw.git -o ~/openclaw
+	fi
+
+	if [[ ! -d /home/linuxbrew ]]; then
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+		brew doctor
+	fi
+	
+	if [[ -f /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	fi
 	
 	if command -v openclaw &>/dev/null; then
 		echo "openclaw is already installed"
-		# openclaw security audit --deep
-		# openclaw doctor --fix
+		openclaw security audit
+		openclaw doctor
+		# openclaw gateway status
+		# openclaw dashboard
 	else
-		curl -fsSL https://openclaw.ai/install.sh | bash
+		# curl -fsSL https://openclaw.ai/install.sh | bash
+		curl -fsSL https://openclaw.ai/install.sh | bash -s -- --install-method git
+		# openclaw onboard --install-daemon
+		# openclaw completion --write-state
+		# openclaw completion -i
+		# openclaw gateway status
+		# openclaw dashboard
 		echo "openclaw installation completed."
 	fi
 
 }
 
+# UNINSTALL_OPENCLAW
 INSTALL_OPENCLAW
